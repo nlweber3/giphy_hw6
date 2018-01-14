a5b6c1b2d68ba7d238f94ec52e0570b425da7a99
nlweber3 <nlweber3@gmail.com>
2018-01-13 20:13:20 -0800

    Add files via upload
    "final commit"

--- assets/java.js
+++ assets/java.js
@@ -10,42 +10,42 @@ for (let i = 0; i < topics.length; i++) {
 }
 $("#submit_button").on("click", function() {
 	topics.push($("#inlineFormInput").val());
-			console.log(topics);
-			var button = $('<button class="button">');
+	console.log(topics);
+	var button = $('<button class="button">');
 	button.text($("#inlineFormInput").val());
 	button.attr('data-hero', $("#inlineFormInput").val());
 	$('#heroes_div').append(button);
 	$("#inlineFormInput").val('');
 
-$(".button").on("click", function() {
+	$(".button").on("click", function() {
 
-	var hero = $(this).attr("data-hero");
-
-
-
-	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-	hero +  "&api_key=EwWNWNoK3EfrkSejjujKMAUkNGKvIzbG&limit=10";
-
-
-	$.ajax({
-		url: queryURL,
-		method: "GET"
-	}).done(function(response) {
-		var results = response;
-		console.log(response);
-
-		for (var i = 0; i < results.data.length; i++) {
-
-
-			$("#gifs").prepend("<img src='" + results.data[i]['images']['fixed_height']['url'] + "' />");
-		}
-
-		
+		var hero = $(this).attr("data-hero");
 
 
 
+		var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+		hero +  "&api_key=EwWNWNoK3EfrkSejjujKMAUkNGKvIzbG&limit=10";
+
+
+		$.ajax({
+			url: queryURL,
+			method: "GET"
+		}).done(function(response) {
+			var results = response;
+			console.log(response);
+
+			for (var i = 0; i < results.data.length; i++) {
+
+
+				$("#gifs").prepend("<img src='" + results.data[i]['images']['fixed_height']['url'] + "' />");
+			}
+
+			
+
+
+
+		});
 	});
-});
 });
 
 
